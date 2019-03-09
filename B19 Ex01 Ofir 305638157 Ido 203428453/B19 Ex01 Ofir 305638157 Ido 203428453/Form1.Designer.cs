6aba47419fd804044391f9b18798f7c40967ceab
Ofir Uzan <ophiruzan3@gmail.com>
2019-03-09 15:40:34 +0200

    ...

--- B19 Ex01 Ofir 305638157 Ido 203428453/B19 Ex01 Ofir 305638157 Ido 203428453/Form1.Designer.cs
+++ B19 Ex01 Ofir 305638157 Ido 203428453/B19 Ex01 Ofir 305638157 Ido 203428453/Form1.Designer.cs
@@ -35,25 +35,33 @@
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
             this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.m_TabPage_MyAlbums = new System.Windows.Forms.TabPage();
+            this.m_buttonPrevoiusPic = new System.Windows.Forms.Button();
+            this.m_buttonNextPic = new System.Windows.Forms.Button();
+            this.m_pictureBoxCurrentPic = new System.Windows.Forms.PictureBox();
+            this.m_comboBoxAlbums = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.m_PB_UserProfilePic)).BeginInit();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
+            this.m_TabPage_MyAlbums.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.m_pictureBoxCurrentPic)).BeginInit();
             this.SuspendLayout();
             // 
             // m_PB_UserProfilePic
             // 
-            this.m_PB_UserProfilePic.Location = new System.Drawing.Point(6, 40);
+            this.m_PB_UserProfilePic.Location = new System.Drawing.Point(9, 62);
+            this.m_PB_UserProfilePic.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.m_PB_UserProfilePic.Name = "m_PB_UserProfilePic";
-            this.m_PB_UserProfilePic.Size = new System.Drawing.Size(164, 181);
+            this.m_PB_UserProfilePic.Size = new System.Drawing.Size(246, 278);
             this.m_PB_UserProfilePic.TabIndex = 0;
             this.m_PB_UserProfilePic.TabStop = false;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(8, 227);
+            this.button1.Location = new System.Drawing.Point(12, 349);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(143, 31);
+            this.button1.Size = new System.Drawing.Size(214, 48);
             this.button1.TabIndex = 1;
             this.button1.Text = "Login";
             this.button1.UseVisualStyleBackColor = true;
@@ -62,18 +70,20 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(6, 24);
+            this.label1.Location = new System.Drawing.Point(9, 37);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.Size = new System.Drawing.Size(86, 20);
             this.label1.TabIndex = 2;
             this.label1.Text = "First Name";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(69, 24);
+            this.label2.Location = new System.Drawing.Point(104, 37);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.Size = new System.Drawing.Size(86, 20);
             this.label2.TabIndex = 3;
             this.label2.Text = "Last Name";
             // 
@@ -81,11 +91,12 @@
             // 
             this.tabControl1.Controls.Add(this.tabPage1);
             this.tabControl1.Controls.Add(this.tabPage2);
-            this.tabControl1.Controls.Add(this.tabPage3);
-            this.tabControl1.Location = new System.Drawing.Point(0, 12);
+            this.tabControl1.Controls.Add(this.m_TabPage_MyAlbums);
+            this.tabControl1.Location = new System.Drawing.Point(0, 18);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(797, 404);
+            this.tabControl1.Size = new System.Drawing.Size(1196, 622);
             this.tabControl1.TabIndex = 4;
             // 
             // tabPage1
@@ -94,46 +105,93 @@
             this.tabPage1.Controls.Add(this.label2);
             this.tabPage1.Controls.Add(this.button1);
             this.tabPage1.Controls.Add(this.label1);
-            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Location = new System.Drawing.Point(4, 29);
+            this.tabPage1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(789, 378);
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.tabPage1.Size = new System.Drawing.Size(1188, 589);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "tabPage1";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
             // tabPage2
             // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Location = new System.Drawing.Point(4, 29);
+            this.tabPage2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(789, 378);
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.tabPage2.Size = new System.Drawing.Size(1188, 589);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "tabPage2";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
-            // tabPage3
+            // m_TabPage_MyAlbums
             // 
-            this.tabPage3.Location = new System.Drawing.Point(4, 22);
-            this.tabPage3.Name = "tabPage3";
-            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage3.Size = new System.Drawing.Size(789, 378);
-            this.tabPage3.TabIndex = 2;
-            this.tabPage3.Text = "tabPage3";
-            this.tabPage3.UseVisualStyleBackColor = true;
+            this.m_TabPage_MyAlbums.Controls.Add(this.m_buttonPrevoiusPic);
+            this.m_TabPage_MyAlbums.Controls.Add(this.m_buttonNextPic);
+            this.m_TabPage_MyAlbums.Controls.Add(this.m_pictureBoxCurrentPic);
+            this.m_TabPage_MyAlbums.Controls.Add(this.m_comboBoxAlbums);
+            this.m_TabPage_MyAlbums.Location = new System.Drawing.Point(4, 29);
+            this.m_TabPage_MyAlbums.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.m_TabPage_MyAlbums.Name = "m_TabPage_MyAlbums";
+            this.m_TabPage_MyAlbums.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.m_TabPage_MyAlbums.Size = new System.Drawing.Size(1188, 589);
+            this.m_TabPage_MyAlbums.TabIndex = 2;
+            this.m_TabPage_MyAlbums.Text = "My Albums";
+            this.m_TabPage_MyAlbums.UseVisualStyleBackColor = true;
+            // 
+            // m_buttonPrevoiusPic
+            // 
+            this.m_buttonPrevoiusPic.Location = new System.Drawing.Point(317, 475);
+            this.m_buttonPrevoiusPic.Name = "m_buttonPrevoiusPic";
+            this.m_buttonPrevoiusPic.Size = new System.Drawing.Size(90, 57);
+            this.m_buttonPrevoiusPic.TabIndex = 3;
+            this.m_buttonPrevoiusPic.Text = "Previous";
+            this.m_buttonPrevoiusPic.UseVisualStyleBackColor = true;
+            this.m_buttonPrevoiusPic.Click += new System.EventHandler(this.m_buttonPrevoiusPic_Click);
+            // 
+            // m_buttonNextPic
+            // 
+            this.m_buttonNextPic.Location = new System.Drawing.Point(606, 475);
+            this.m_buttonNextPic.Name = "m_buttonNextPic";
+            this.m_buttonNextPic.Size = new System.Drawing.Size(90, 57);
+            this.m_buttonNextPic.TabIndex = 2;
+            this.m_buttonNextPic.Text = "Next";
+            this.m_buttonNextPic.UseVisualStyleBackColor = true;
+            this.m_buttonNextPic.Click += new System.EventHandler(this.buttonNext_Click);
+            // 
+            // m_pictureBoxCurrentPic
+            // 
+            this.m_pictureBoxCurrentPic.Location = new System.Drawing.Point(317, 168);
+            this.m_pictureBoxCurrentPic.Name = "m_pictureBoxCurrentPic";
+            this.m_pictureBoxCurrentPic.Size = new System.Drawing.Size(379, 287);
+            this.m_pictureBoxCurrentPic.TabIndex = 1;
+            this.m_pictureBoxCurrentPic.TabStop = false;
+            // 
+            // m_comboBoxAlbums
+            // 
+            this.m_comboBoxAlbums.FormattingEnabled = true;
+            this.m_comboBoxAlbums.Location = new System.Drawing.Point(21, 51);
+            this.m_comboBoxAlbums.Name = "m_comboBoxAlbums";
+            this.m_comboBoxAlbums.Size = new System.Drawing.Size(294, 28);
+            this.m_comboBoxAlbums.TabIndex = 0;
+            this.m_comboBoxAlbums.SelectedIndexChanged += new System.EventHandler(this.m_comboBoxAlbums_SelectedIndexChanged);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1200, 692);
             this.Controls.Add(this.tabControl1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.m_PB_UserProfilePic)).EndInit();
             this.tabControl1.ResumeLayout(false);
             this.tabPage1.ResumeLayout(false);
             this.tabPage1.PerformLayout();
+            this.m_TabPage_MyAlbums.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.m_pictureBoxCurrentPic)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -147,7 +205,11 @@
         private System.Windows.Forms.TabControl tabControl1;
         private System.Windows.Forms.TabPage tabPage1;
         private System.Windows.Forms.TabPage tabPage2;
-        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage m_TabPage_MyAlbums;
+        private System.Windows.Forms.Button m_buttonNextPic;
+        private System.Windows.Forms.PictureBox m_pictureBoxCurrentPic;
+        private System.Windows.Forms.ComboBox m_comboBoxAlbums;
+        private System.Windows.Forms.Button m_buttonPrevoiusPic;
     }
 }
 
